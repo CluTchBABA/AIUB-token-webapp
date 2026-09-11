5b9d95d775af007c3279686c146a596d90e1da95
Faysal alam <faysalalam525@gmail.com>
2026-09-11 22:10:44 +0600

    Add room and teacher assignment methods

--- model/rooms.php
+++ model/rooms.php
@@ -48,4 +48,41 @@ class Rooms
         $stmt->execute();
         return $stmt->get_result()->fetch_assoc();
     }
+
+    public function get_room_by_supervisor(int $supervisor_id): false|array|null
+    {
+        $stmt=$this->conn->prepare("SELECT *FROM rooms WHERE supervisor_id=?");
+        $stmt->bind_param("i",$supervisor_id);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function get_techers_in_room(int $room_id): false|array|null
+    {
+        $stmt=$this->conn->prepare("
+        SELECT u.fullname,u.uni_id
+        FROM teacher_assignment ta
+        JOIN users u ON ta.user_id =u.id
+        WHERE ta.room_id =?
+        ");
+        $stmt->bind_param("i",$room_id);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    PUBLIC function assign_teacher_to_room(int $teacher_id, int $room_id): bool
+    {
+        $check=$this->conn->prepare(@SELECT user_id FROM teacher_assignment WHERE user_id=?");
+        $check->bind_param("i",$teacher_id);
+        "check->execute();
+        if("check->get_result()->fetch_assoc()){
+        return false;
+        }
+
+        $stmt =$this->conn->prepare("INSERT INTO teacher_assignment(user_id,room_id) VALUES (?,?)");
+        "stmt->bind_param("ii", "teacher_id.$room_id);
+        return $stmt->execute();
+
+     }
+     
 }
