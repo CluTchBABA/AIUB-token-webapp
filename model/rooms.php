8ea4b636a213684521101938625c4d3c244c1658
Faysal alam <faysalalam525@gmail.com>
2026-09-14 01:25:51 +0600

    Remove removeTeacherFromRoom method
    Removed the method for removing a teacher from a room.

--- model/rooms.php
+++ model/rooms.php
@@ -69,12 +69,6 @@ class Rooms
         $stmt->execute();
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
-    public function removeTeacherFromRoom($teacher_id, $room_id) {
-	    $sql = "DELETE FROM teacher_assignment WHERE user_id = ? AND room_id = ?";
-	    $prepared_statement = $this->conn->prepare($sql);
-	    $prepared_statement->bind_param('ii', $teacher_id, $room_id);
-	    return $prepared_statement->execute();
-      }
     
     public function assign_teacher_to_room(int $teacher_id, int $room_id): bool
     {
